557bafa946c4f7bd40980c1a8b7edf7570b34d3a
Jason Lecomte <LecomteJason@hotmail.com>
2023-06-27 12:11:58 +0200

    Add Mapper Ticket Update CreateTicketDTO t

--- HelpDesk_Labo/DTOs/CreateTicketDTO.cs
+++ HelpDesk_Labo/DTOs/CreateTicketDTO.cs
@@ -1,20 +1,29 @@
 ﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace API_HelpDesk_Labo.DTOs
 {
     public class CreateTicketDTO
     {
+        [Required]
         public string Title { get; set; }
 
         public string Description { get; set; }
 
         public string Attachment { get; set; }
 
+        [Required]
         public TicketType TicketType { get; set; }
 
-        public DateTime StartDate { get; set; } = DateTime.Now;
+        private readonly DateTime _startDate = DateTime.Now;
 
-        
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+        }
+             
+         
+
 
     }
 }
---
+++ HelpDesk_Labo/Mappers/TicketMapper.cs
@@ -0,0 +1,20 @@
+﻿using API_HelpDesk_Labo.DTOs;
+using Domain.Entities;
+using System.Runtime.InteropServices;
+
+namespace API_HelpDesk_Labo.Mappers
+{
+    public static class TicketMapper
+    {
+        public static Ticket ToTicket(this CreateTicketDTO ticketDTO)
+        {
+            return new Ticket
+            {
+                Title = ticketDTO.Title,
+                Description = ticketDTO.Description,
+                Attachment = ticketDTO.Attachment,
+                StartDate = ticketDTO.StartDate,
+            };
+        }
+    }
+}
